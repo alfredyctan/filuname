a24ae858ce4a767a38b61397458414dce378be82
Alfred Tan <alfred.yctan@gmail.com>
2017-10-20 02:18:37 +0800

    skip scanning the resource from main start up class

--- filuname/src/test/java/org/alf/filuname/IDEFilunameLauncher.java
+++ filuname/src/test/java/org/alf/filuname/IDEFilunameLauncher.java
@@ -3,6 +3,8 @@ package org.alf.filuname;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource({
@@ -14,6 +16,14 @@ import org.springframework.context.annotation.ImportResource;
 	"classpath:/exclusion-dao.xml",
 	"classpath:/hitcount-dao.xml"
 })
+@ComponentScan(
+	excludeFilters = {
+		@ComponentScan.Filter(
+			type = FilterType.ASSIGNABLE_TYPE, 
+			value = org.alf.filuname.FilunameLauncher.class
+		) 
+	}
+)
 @SpringBootApplication
 public class IDEFilunameLauncher extends SpringBootServletInitializer {
 
